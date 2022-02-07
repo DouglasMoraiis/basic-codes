ab32d9f0f6f61a8922fbf26df4739655d24a81a5
Douglas Morais <janderdouglas2014@gmail.com>
2022-02-06 22:54:47 -0300

    add api-rest-asp-dotnet

---
+++ api-rest-asp-dotnet/api-rest-asp-dotnet/Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace api_rest_asp_dotnet.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ api-rest-asp-dotnet/api-rest-asp-dotnet/Domain/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace api_rest_asp_dotnet.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+    }
+}
