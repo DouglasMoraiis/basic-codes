57715d6ea758eb6ef196b3578f73972f2f15e6de
Douglas Morais <janderdouglas2014@gmail.com>
2022-02-06 22:56:47 -0300

    Remove api rest

--- api-rest-asp-dotnet/api-rest-asp-dotnet/Domain/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api_rest_asp_dotnet.Domain.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public IList<Product> Products { get; set; } = new List<Product>();
-    }
-}
--- api-rest-asp-dotnet/api-rest-asp-dotnet/Domain/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace api_rest_asp_dotnet.Domain.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        
-    }
-}
