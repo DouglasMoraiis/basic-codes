5a7c7ba990439dd85bb5cda84261d4c99cbbdb7c
Douglas Morais <douglasmoraisdev@gmail.com>
2022-02-11 17:05:13 -0300

    feat: Add novos exemplos

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using Delegates.Services;
+using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        /*
+         * São usados para passar metodos como parametros de outros métodos. Semelhando ao que fazemos em Javascript quando queremos passar uma função de callback para outra função.
+        */
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            Console.WriteLine(op(a, b));
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2020, 12, 14, 20, 1, 15);
+            Console.WriteLine(dateTime.ElapsedTime());
+        }
+    }
+}
